95e4496b05881162ffc3e125544ab09f895691cc
DoubleH-pku <2400016629hh@gmail.com>
2025-09-16 22:38:50 +0800

    Finish lab4.

--- lab4/flik/TestFilk.java
+++ lab4/flik/TestFilk.java
@@ -8,7 +8,7 @@ public class TestFilk {
     public void testFilk() {
         int j = 0;
         for (int i = 0; i < 1000; i++) {
-            assertEquals(i, j);
+            assertTrue(i + " doesn't equals to " + j,Flik.isSameNumber(i, j));
             j += 1;
         }
     }
